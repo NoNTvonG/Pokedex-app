e5368e44e6ccda8a0fce2afe6cd5f99bbbc5c659
Roman Pyrih <NoNTvonG@gmail.com>
2023-12-29 09:50:49 +0100

    Make pokemon single card

--- src/components/PokemonCard/PokemonCard.tsx
+++ src/components/PokemonCard/PokemonCard.tsx
@@ -16,15 +16,15 @@ export const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
   };
 
   return (
-    <div className={style.card}>
+    <div className={`${style.card} ${style[pokemon.types[0].type.name]}`}>
       <div className={style.card_info}>
         <p className={style.card_id}>Nº{formatId(pokemon.id)}</p>
         <h3 className={style.name}>{capitalizeFirstLetter(pokemon.name)}</h3>
         <ul className={style.card_types}>
           {pokemon.types.map((type) => (
-            <li className={style.card_type}>
+            <li className={`${style.card_type} ${style[type.type.name]}`}>
               <div className={style.card_type_icon}>
-                <img src="" alt="" />
+                <img src={`/src/assets/images/elements/${type.type.name}.svg`} alt="" />
               </div>
               <p className={style.card_type_name}>
                 {capitalizeFirstLetter(type.type.name)}
@@ -33,8 +33,9 @@ export const PokemonCard: React.FC<PokemonCardProps> = ({ pokemon }) => {
           ))}
         </ul>
       </div>
-      <div className={style.card_img}>
-        <img src={pokemon.sprites.other.showdown.front_default} alt={pokemon.name} />
+      <div className={`${style.card_img} ${style[pokemon.types[0].type.name]}`}>
+        <img src={pokemon.sprites.other.showdown.front_default} className={style.card_img_pokemon} alt={pokemon.name} />
+        <img src={`/src/assets/images/elements_mono/${pokemon.types[0].type.name}.svg`}className={style.card_img_type} alt={style[pokemon.types[0].type.name]} />
       </div>
     </div>
   )
