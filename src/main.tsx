44ba262f4336a842d7469ae6893ba796ff5c2070
Roman Pyrih <NoNTvonG@gmail.com>
2023-12-29 11:19:15 +0100

    Add footer

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { Home } from './pages/Home.tsx'
 import './assets/styles/index.scss'
 import { Header } from './components/Header/Header.tsx'
+import { Footer } from './components/Footer/Footer.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header />
     <Home />
+    <Footer />
   </React.StrictMode>,
 )
